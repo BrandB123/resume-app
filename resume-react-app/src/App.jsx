a4938e98e87936506d9f4582cec2260480a6f94d
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-07-30 23:35:43 -0500

    Layout initial design

--- resume-react-app/src/App.jsx
+++ resume-react-app/src/App.jsx
@@ -1,35 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const mockData = {
+
+}
+
+
+export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sidebar />
+      <Document />
     </>
   )
 }
 
-export default App
+function Sidebar(){
+  return(
+    <div
+      className='sidebar'
+    >
+      <button>Personal Information</button>
+      <button>Education</button>
+      <button>Work Experience</button>
+      <button>Skills</button>
+    </div>
+  );
+}
+
+
+function Document(){
+  return(
+    <div
+      className='document'
+    >
+      <div className='leftPanel'>
+        <div className='name'>Brandon Bartlett</div>
+        <div className='contactInfo'>
+          <p>Email: test@testemail.com</p>
+          <p>Phone: 555-555-5555</p>
+          <p>Address: 123 Test St, City, State 12345</p>
+        </div>
+        <div className='socialMedia'>
+          <p>Github</p>
+          <p>LinkedIn</p>
+        </div>
+      </div>
+
+      <div className='rightPanel'>
+        <div className='documentSectionHeading education '>
+          Education
+          <p className='documentSectionContent'>University of Missouri - Columbia</p>
+          <p className='documentSectionContent'><i>BA Political Science</i></p>
+          <p className='documentSectionContent'>2017 - 2021</p>
+        </div>
+
+        <div className='documentSectionHeading workExperience'>
+          Work Experience
+          <p className='documentSectionContent'>SuretyBonds.com</p>
+          <p className='documentSectionContent'>Web Developer</p>
+          <p className='documentSectionContent'>2020 - Present</p>
+        </div>
+
+        <div className='documentSectionHeading skills'>
+          Skills
+          <p className='documentSectionContent'>Web Development:</p>
+            <p className='documentSectionContent'>HMTL, CSS, ES6 JavaScript, NPM, React</p>
+
+        </div>
+      </div>
+    </div>
+  );
+}
