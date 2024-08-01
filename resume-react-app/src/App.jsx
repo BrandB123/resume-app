6c2237862fdf8579a330a2f69d4aa591b045eefb
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-07-31 23:13:57 -0500

    Complete initial component implementation

--- resume-react-app/src/App.jsx
+++ resume-react-app/src/App.jsx
@@ -3,8 +3,7 @@ import './App.css'
 
 
 const MOCKDATA = {
-    firstName: 'John',
-    lastName: 'Smith',
+    name: 'John Smith',
     email: 'johnsmith@email.com',
     phone: '505-500-5005',
     address: '916 N Jefferson St, Carrollton, Mo 64633',
@@ -17,12 +16,11 @@ const MOCKDATA = {
       titleOfStudy: 'Bachelors of Arts in Fake Data',
       startDate: '2017',
       endDate: '2021',
-      id: '1'
     }],
     workExperience: [
-      {employer: 'employer1', position: 'position1', startDate: '2022', endDate: 'present', id: '1'},
-      {employer: 'employer2', position: 'position2', startDate: '2019', endDate: '2022', id: '2'},
-      {employer: 'employer3', position: 'position3', startDate: '2015', endDate: '2019', id: '3'}
+      {employer: 'employer1', position: 'position1', startDate: '2022', endDate: 'present', summary: 'summary1'},
+      {employer: 'employer2', position: 'position2', startDate: '2019', endDate: '2022', summary: 'summary2'},
+      {employer: 'employer3', position: 'position3', startDate: '2015', endDate: '2019', summary: 'summary3'}
     ],
     skills: [
       {title: 'Web Development', description: 'Proficient in HMTL, CSS, Javascript, Sveltekit, TypeScript, Express'},
@@ -41,50 +39,104 @@ export default function App({ data = {MOCKDATA} }) {
   )
 }
 
+
 function Sidebar(){
   return(
-    <div
-      className='sidebar'
-    >
-      <button>Personal Information</button>
-      <button>Education</button>
-      <button>Work Experience</button>
-      <button>Skills</button>
+    <div className='sidebar'>
+      <PersonalInformationInput />
+      <EducationInformationInput />
+      <WorkInformationInput />
+      <SkillsInput />
     </div>
+  );
+}
+
+
+function PersonalInformationInput(){
+  return(
+    <div className='personalInfoInput'>
+      <InputDropdownButton title='Personal Information'/>
+      <FormInput label='Name'/>
+      <FormInput label='Email'/>
+      <FormInput label='Phone'/>
+      <FormInput label='Address'/>
+    </div>
+  );
+}
+
+
+function EducationInformationInput(){
+  return(
+    <div className='educationInput'>
+      <InputDropdownButton title='Education Information'/>
+      <FormInput label='School'/>
+      <FormInput label='Degree'/>
+      <FormInput label='Start Date'/>
+      <FormInput label='End Date'/>
+      <AddButton />
+    </div>
+  );
+}
+
+
+function WorkInformationInput(){
+  return(
+    <div className='employmentInput'>
+      <InputDropdownButton title='Employment History'/>
+      <FormInput label='Employer'/>
+      <FormInput label='Position'/>
+      <FormInput label='Start Date'/>
+      <FormInput label='End Date'/>
+      <FormInput label='Summary of Responsibilities'/>
+      <AddButton />
+    </div>
+  );
+}
+
+
+function SkillsInput(){
+  return(
+    <div className='skillInput'>
+      <InputDropdownButton title='Skills'/>
+      <FormInput label='Skill'/>
+      <FormInput label='Description'/>
+      <AddButton />
+    </div>
+  );
+}
+
+
+function InputDropdownButton({title, handleClick}){
+  return(
+    <button onClick={handleClick} className='dropdownButton'>
+      {title}
+    </button>
+  );
+}
+
+function FormInput({label, dataField, handleChange}){
+  return(
+  <form>
+    <label htmlFor='{label}' style={{display: 'block'}}>{ label }</label>
+    <input id='{label}' onChange={() => handleChange(dataField)}></input>
+  </form>
+  );
+}
+
+function AddButton({handleClick}){
+  return(
+    <button onClick={handleClick}>
+      +
+    </button>
   );
 }
 
 
 function Document({ data }){
   return(
-    <div
-      className='document'
-    >
+    <div className='document'>
       <LeftPanel data = {data}/>
-
-      <div className='rightPanel'>
-        <div className='documentSectionHeading education'>
-          Education
-          <p className='documentSectionContent'>{data.education.institution}</p>
-          <p className='documentSectionContent'><i>{data.education.titleOfStudy}</i></p>
-          <p className='documentSectionContent'>{data.education.startDate} - {data.education.endDate}</p>
-        </div>
-
-        <div className='documentSectionHeading workExperience'>
-          Work Experience
-          <p className='documentSectionContent'>Not filled</p>
-          <p className='documentSectionContent'>Not filled</p>
-          <p className='documentSectionContent'>Not filled</p>
-        </div>
-
-
-        <div className='documentSectionHeading skills'>
-          Skills
-          <p className='documentSectionContent'>Web Development:</p>
-            <p className='documentSectionContent'>HMTL, CSS, ES6 JavaScript, NPM, React</p>
-
-        </div>
-      </div>
+      <RightPanel data = {data}/>
     </div>
   );
 }
@@ -93,46 +145,62 @@ function Document({ data }){
 function LeftPanel({ data }){
   return(
     <div className='leftPanel'>
-      <div className='name'>{ data.firstName + " " + data.lastName }</div>
+      <div className='name'>{ data.name }</div>
       <div className='contactInfo'>
         <PersonalInformationSection title='Email:' content={ data.email }/>
         <PersonalInformationSection title='Phone:' content={ data.phone }/>
         <PersonalInformationSection title='Address:' content={ data.address }/>
-      </div>
-      <div className='socialMedia'>
-        <PersonalInformationSection title='Github:' content={ data.socialMedia.github }/>
-        <PersonalInformationSection title='LinkedIn:' content={ data.socialMedia.linkedIn }/>
       </div>
     </div>
   );
 }
 
 
-function RightPanel({ data }){
+function RightPanel({data}){
   return(
-    <div className='rightPanel'>
-        <div className='documentSectionHeading education '>
-          Education
-          <p className='documentSectionContent'>{data.education.institution}</p>
-          <p className='documentSectionContent'><i>{data.education.titleOfStudy}</i></p>
-          <p className='documentSectionContent'>{data.education.startDate} - {data.education.endDate}</p>
-        </div>
-
-        <div className='documentSectionHeading workExperience'>
-          Work Experience
-          <p className='documentSectionContent'>Not filled</p>
-          <p className='documentSectionContent'>Not filled</p>
-          <p className='documentSectionContent'>Not filled</p>
-        </div>
-
-
-        <div className='documentSectionHeading skills'>
-          Skills
-          <p className='documentSectionContent'>Web Development:</p>
-            <p className='documentSectionContent'>HMTL, CSS, ES6 JavaScript, NPM, React</p>
-
-        </div>
+    <div className='rightPanel' data={data}>
+      <HistoryList 
+	      data={data.education}
+	      heading='Education' 
+	      classTitle='education'
+      />
+      <HistoryList
+	      data={data.workExperience}
+	      heading='Work Experience'
+	      classTitle='workExperience'
+      />
+      <div className='documentSectionHeading'>
+        Skills
+        <ul>
+          {data.skills.map((skill) => {
+            return (
+              <li key={skill.title} skill={skill} className='documentSectionContent'>
+                <PersonalInformationSection 
+                  title={skill.title}
+                  content={skill.description}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
+    </div>
+  );
+}
+
+
+function HistoryList({data, heading, classTitle}){
+  return (
+    <div className={'documentSectionHeading ' + classTitle}>
+      {heading}
+      <ul>
+        {data.map(historyItem => (
+	        classTitle === 'education' ? 
+            <EducationHistory key={historyItem.institution} data={historyItem}/> : 
+	          <WorkHistory key={historyItem.employer} data={historyItem}/>
+        ))}
+      </ul>
+    </div>
   );
 }
 
@@ -146,14 +214,24 @@ function PersonalInformationSection({title, content}){
   );
 }
 
-function educationInformation(){
+
+function EducationHistory({data}){
   return(
-    null
+    <li className='documentSectionContent'>
+      <p>{data.institution}</p>
+      <p>{data.titleOfStudy}</p>
+      <p>{data.startDate} - {data.endDate}</p>
+    </li>
   );
 }
 
-function workExperienceInformation(){
+
+function WorkHistory({data}){
   return(
-    null
+    <li className='documentSectionContent'>
+	    <p>{data.employer} {data.position}</p>
+	    <p>{data.startDate} - {data.endDate}</p>
+	    <p>{data.summary}</p>
+    </li>
   );
 }
